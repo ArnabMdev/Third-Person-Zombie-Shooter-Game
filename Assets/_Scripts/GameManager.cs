c193b67c1526a4e9aaa941931535f20e0daefb03
ArnabMdev <mondalarnab15@gmail.com>
2022-12-23 16:57:51 +0530

    Temp commit

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -1,18 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class GameManager : MonoBehaviour
+
+namespace com.Arnab.ZombieAppocalypseShooter
 {
-    // Start is called before the first frame update
-    void Start()
+    public enum Scene
     {
-        
-    }
+        MainMenu = 0,
+        TestScene = 1,
+        GameLevel = 2,
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
+    public class GameManager : MonoBehaviour
+    {
+        private static GameManager _Instance;
+        public static GameManager Instance
+        {
+            get
+            {
+                if (!_Instance)
+                {
+                    var prefab = Resources.Load<GameObject>("GameManager");
+                    var inScene = Instantiate(prefab);
+                    _Instance = inScene.GetComponentInChildren<GameManager>();
+                    if (!_Instance) _Instance = inScene.AddComponent<GameManager>();
+                    DontDestroyOnLoad(_Instance.transform.root.gameObject);
+                }
+                return _Instance;
+            }
+        }
+
+        [SerializeField] private GameObject _playerPrefab;
+        public AsyncOperation _loadingOperation;
+        public UIManager uiManager;
+        private GameObject _player;
+
+        private void Awake()
+        {
+            SceneManager.sceneLoaded += (s, e) => SpawnPlayer(s, Vector3.zero, quaternion.identity);
+            uiManager = FindObjectOfType<UIManager>();
+        }
+        public void ChangeScene(int sceneIndex)
+        {
+            _loadingOperation = SceneManager.LoadSceneAsync(sceneIndex);
+            
+        }
+        public void SpawnPlayer(UnityEngine.SceneManagement.Scene scene, Vector3 position, Quaternion rotation)
+        {
+            if(scene.name == Scene.MainMenu.ToString())
+            {
+                return;
+            }
+            _player = Instantiate(_playerPrefab, position, rotation);
+        }
+
+    } 
 }
---
+++ Assets/_Scripts/UIManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+namespace com.Arnab.ZombieAppocalypseShooter
+{
+    public class UIManager : MonoBehaviour
+    {
+        [SerializeField] private Slider _progressBar;
+        [SerializeField] private GameObject _loadingScreen;
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        }
+
+        public void ChangeScene()
+        {
+            _loadingScreen.SetActive(true);
+            GameManager.Instance.ChangeScene(1);
+            StartCoroutine(loadingScene());
+        }
+
+        private IEnumerator loadingScene()
+        {
+            while (true)
+            {
+                _progressBar.value = Mathf.Clamp01(GameManager.Instance._loadingOperation.progress / 0.9f);
+                yield return new WaitForEndOfFrame();
+            }
+            
+        }
+    } 
+}
